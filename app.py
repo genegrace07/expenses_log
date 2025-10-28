19742414c1b241ff4646f34cbb4770f8ef4e553b
mark anthony dador <dador.markanthony@gmail.com>
2025-10-28 21:02:50 +0800

    add try again function and for calculation funtion

--- app.py
+++ app.py
@@ -2,51 +2,104 @@ import json
 import datetime
 
 class Expenses:
-    def __init__(self,type,amount):
-        self.type = type
+    def __init__(self,description,amount):
+        self.description = description
         self.amount = amount
-        self.expenses = "logs.json"
-        self.temp_expenses_list = []
-        self.date_today = str(datetime.date.today())
-        self.numbering = 0
-    def input(self):
-        print('=========== DAILY EXPENSES ===========')
-        print('Choose:\n1. Income\n2. Outcome')
-        self.choosen = input('> ')
-    def validate(self):
-        if self.choosen == '1':
-            self.expense()
-        elif self.choosen == '2':
-            print('test')
-        else:
-            print('Invalid option')
-    def expense(self):
-        description = input('Description: ')
-        your_amounts = input('Amount: ')
-        self.numbering += 1
-        new_entry = {"No.":self.numbering,"Description":description,"Amount":your_amounts,"Date":self.date_today}
 
+        self.logs = 'logs.json'
+        self.temp_logs = []
+        self.datetoday = str(datetime.date.today())
+
+    def display(self):
+        print('======= DAILY EXPENSES LOG =======')
+        print('1. Income\n2. Expenses\n3. View logs')
+
+    def user_input(self):
+            selected_number = {'1':'Income','2':'Expense'}
+            self.user_select = input('> ')
+
+            if self.user_select == "1":
+                print(f'{selected_number["1"]} selected')
+                self.user_description = input('Description: ')
+                while True:
+                    try:
+                        self.user_amount = input('Amount: $')
+                        self.user_amount = int(self.user_amount)
+                        self.store()
+                        self.try_again()
+                        break
+                    except ValueError:
+                        print('Invalid amount')
+            elif self.user_select == "2":
+                print(f'{selected_number["2"]} selected')
+                self.user_description = input('Description: ')
+                while True:
+                    try:
+                        self.user_amount = input('Amount: $')
+                        self.user_amount = int(self.user_amount)
+                        self.store()
+                        self.try_again()
+                        break
+                    except ValueError:
+                        print('Invalid amount')
+            elif self.user_select == "3":
+                if hasattr(self,"view_total"):
+                    self.view_total()
+                else:
+                    print('No such a method')
+            else:
+                print('Invalid selected')
+                self.display()
+                self.user_input()
+                print()
+
+    def store(self):
+        self.type_choosen = "income" if self.user_select == "1" else "expense"
 
         try:
-            with open(self.expenses, "r") as f:
-                data = json.load(f)
-                print(data)
-        except json.JSONDecodeError:
-            data = []
+            with open(self.logs, 'r') as f:
+                self.temp_logs = json.load(f)
 
-            data.append(new_entry)
-            self.temp_expenses_list.append(data)
+            new_entry = {"No":len(self.temp_logs)+1, "Type":self.type_choosen ,"Description":self.user_description, "Amount":self.user_amount, "Date":self.datetoday}
+            self.temp_logs.append(new_entry)
 
-        with open(self.expenses, "w+") as f:
-            json.dump(self.temp_expenses_list,f,indent=4)
-            print(f.read())
+            with open(self.logs,'w') as f:
+                json.dump(self.temp_logs,f,indent=4)
 
+        except (json.JSONDecodeError, FileNotFoundError):
+            data = [{"No":len(self.temp_logs)+1, "Type":self.type_choosen, "Description":self.user_description, "Amount":self.user_amount, "Date":self.datetoday}]
+            with open(self.logs,'w') as f:
+                json.dump(data,f,indent=4)
 
+    def try_again(self):
+            print()
+            again = input('Add another? "y" or any key to quit: ')
+            is_running = True
+            print()
+            if again.lower() == 'y':
+                self.display()
+                self.user_input()
+            else:
+                print('Exiting....')
 
+class Calculate(Expenses):
+    def view_total(self):
+        with open(self.logs, 'r') as f:
+            self.temp_logs = json.load(f)
 
-        #print(self.temp_expenses_list) #check if storing successfully
+        total_income = sum(l['Amount'] for l in self.temp_logs if l['Type'] == 'income' )
+        total_expenses = sum(l['Amount'] for l in self.temp_logs if l['Type'] == 'expense')
 
-user = Expenses(0,0)
-user.input()
-user.validate()
+        print('===== VIEW DAILY EXPENSES LOG =====')
+        print('Type'.rjust(10),'  |','Amount'.rjust(8),'  |','Date'.rjust(7))
+        for items in self.temp_logs:
+            print(f"{items['No']:<4} {items['Type']:<9} ${items['Amount']:<10,.2f} {items['Date']}")
 
+        print()
+        print(f'Total Income: ${total_income:,.2f}')
+        print(f'Total Expenses: ${total_expenses:,.2f}')
+        print(f'Grand total: ${total_expenses+total_income:,.2f}')
+
+user = Calculate('description=','amount=')
+user.display()
+user.user_input()
